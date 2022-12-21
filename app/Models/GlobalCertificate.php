041ff260ddc7b89009e1b7a24d24322f2b81e1a7
lamptechs <abdulkaderzilani98@gmail.com>
2022-12-21 15:15:23 +0600

    Global Certificate

--- app/Models/GlobalCertificate.php
+++ app/Models/GlobalCertificate.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class VendorCertificate extends Model
+class GlobalCertificate extends Model
 {
     use HasFactory;
 
