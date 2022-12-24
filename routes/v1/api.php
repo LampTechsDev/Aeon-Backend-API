7184995beea229f347952c6cbc159c3020d27cff
lamptechs <abdulkaderzilani98@gmail.com>
2022-12-24 16:52:52 +0600

    vandor manufacturer updated latest

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -36,17 +36,6 @@ use App\Http\Controllers\V1\Admin\VendorManufacturerController;
 |
 */
 
-/**
- *Vendor Manufacturer Section
-*/
-Route::prefix('vendor_manufacturer')->group(function(){
-
-    Route::get('/list',         [VendorManufacturerController::class, 'index']);
-    Route::get('/show',         [VendorManufacturerController::class, "show"]);
-    Route::post('/store',       [VendorManufacturerController::class, "store"]);
-    Route::post('/update/{id}', [VendorManufacturerController::class, "update"]);
-    Route::post('/delete/{id}', [VendorManufacturerController::class, "destroy"]);
-});
 
 
 /**
@@ -322,6 +311,19 @@ Route::middleware(["auth:admin"])->prefix('admin')->group(function(){
         Route::post('/update/{id}', [GlobalCertificateController::class, "update"]);
         Route::post('/delete/{id}', [GlobalCertificateController::class, "destroy"]);
     });
+
+/**
+ *Vendor Manufacturer Section
+*/
+Route::prefix('vendor_manufacturer')->group(function(){
+
+    Route::get('/list',         [VendorManufacturerController::class, 'index']);
+    Route::get('/show',         [VendorManufacturerController::class, "show"]);
+    Route::post('/store',       [VendorManufacturerController::class, "store"]);
+    Route::post('/update/{id}', [VendorManufacturerController::class, "update"]);
+    Route::post('/delete/{id}', [VendorManufacturerController::class, "destroy"]);
+});
+
 
 
 
