e552e980d8558bc61a479060e12dd1976e85e08f
LamTechsDev <dev@lamptechs.com>
2023-01-05 16:13:04 +0600

    Bulk Fabric Information Added

---
+++ app/Http/Controllers/V1/Admin/BulkFabricInformationController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BulkFabricInformationResource;
+use App\Models\BulkFabricInformation;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Exception;
+
+class BulkFabricInformationController extends Controller
+{
+    public function store(Request $request){
+        try{
+
+            // if(!PermissionController::hasAccess("group_create")){
+            //     return $this->apiOutput("Permission Missing", 403);
+            // }
+
+            $validator = Validator::make( $request->all(),[
+                // 'name'          => ["required", "min:4"],
+                // 'description'   => ["nullable", "min:4"],
+            ]);
+                
+            if ($validator->fails()) {    
+                $this->apiOutput($this->getValidationError($validator), 400);
+            }
+   
+            $bulkFabricInformation = new BulkFabricInformation();
+            $bulkFabricInformation->po_number = $request->po_number ;
+            $bulkFabricInformation->po_id = $request->po_id;
+            $bulkFabricInformation->fabric_ordered_plan = $request->fabric_ordered_plan;
+            $bulkFabricInformation->fabric_ordered_actual = $request->fabric_ordered_actual;
+            $bulkFabricInformation->bulk_fabric_knit_down_approval_plan = $request->bulk_fabric_knit_down_approval_plan;
+            $bulkFabricInformation->bulk_fabric_knit_down_approval_actual = $request->bulk_fabric_knit_down_approval_actual;
+            $bulkFabricInformation->bulk_fabric_knit_down_dispatch_details = $request->bulk_fabric_knit_down_dispatch_details;
+            $bulkFabricInformation->bulk_fabric_knit_down_dispatch_sending_date = $request->bulk_fabric_knit_down_dispatch_sending_date;
+            $bulkFabricInformation->bulk_fabric_knit_down_dispatch_aob_number = $request->bulk_fabric_knit_down_dispatch_aob_number;
+            $bulkFabricInformation->bulk_yarn_fabric_inhouse_plan = $request->bulk_yarn_fabric_inhouse_plan;
+            $bulkFabricInformation->bulk_yarn_fabric_inhouse_actual = $request->bulk_yarn_fabric_inhouse_actual;
+
+            $bulkFabricInformation->save();
+            $this->apiSuccess();
+            $this->data = (new BulkFabricInformationResource($bulkFabricInformation));
+            return $this->apiOutput("BulkFabricInformation Added Successfully");
+        }catch(Exception $e){
+            return $this->apiOutput($this->getError( $e), 500);
+        }
+    }
+}
---
+++ database/migrations/2023_01_05_083840_create_bulk_fabric_information_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBulkFabricInformationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bulk_fabric_information', function (Blueprint $table) {
+            $table->id();
+            $table->string("po_number");
+            $table->integer("po_id");
+            $table->date("fabric_ordered_plan");
+            $table->date("fabric_ordered_actual");
+            $table->date("bulk_fabric_knit_down_approval_plan");
+            $table->date("bulk_fabric_knit_down_approval_actual");
+            $table->text("bulk_fabric_knit_down_dispatch_details");
+            $table->date("bulk_fabric_knit_down_dispatch_sending_date");
+            $table->string("bulk_fabric_knit_down_dispatch_aob_number");
+            $table->date("bulk_yarn_fabric_inhouse_plan");
+            $table->date("bulk_yarn_fabric_inhouse_actual");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bulk_fabric_information');
+    }
+}
