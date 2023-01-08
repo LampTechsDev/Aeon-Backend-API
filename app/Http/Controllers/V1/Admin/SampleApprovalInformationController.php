12c7b8b9b59d7c7e0f5bba91431157c44dd76b1d
LamTechsDev <dev@lamptechs.com>
2023-01-08 20:55:38 +0600

    Sample Approval Completed

--- app/Http/Controllers/V1/Admin/SampleApprovalInformationController.php
+++ app/Http/Controllers/V1/Admin/SampleApprovalInformationController.php
@@ -16,6 +16,20 @@ use Exception;
 
 class SampleApprovalInformationController extends Controller
 {
+    public function index()
+    {
+       try{
+    
+            $this->data = SampleApprovalInformationResource::collection(SampleApprovalInformation::all());
+            $this->apiSuccess("Sample Approval Information Loaded Successfully");
+            return $this->apiOutput();
+
+        }catch(Exception $e){
+            return $this->apiOutput($this->getError($e), 500);
+        }
+    }
+
+
     public function store(Request $request){
         try{
 
@@ -130,5 +144,70 @@ class SampleApprovalInformationController extends Controller
             $data->type = $request->pp_sample_image_type;
             $data->save();
         }
+    }
+
+    public function update(Request $request){
+        try{
+
+            // if(!PermissionController::hasAccess("group_create")){
+            //     return $this->apiOutput("Permission Missing", 403);
+            // }
+
+            $validator = Validator::make( $request->all(),[
+                // 'name'          => ["required", "min:4"],
+                // 'description'   => ["nullable", "min:4"],
+            ]);
+                
+            if ($validator->fails()) {    
+                $this->apiOutput($this->getValidationError($validator), 400);
+            }
+   
+            $sampleApproval = SampleApprovalInformation::find($request->id);
+            $sampleApproval->development_photo_sample_sent_plan = $request->development_photo_sample_sent_plan;
+            $sampleApproval->development_photo_sample_sent_actual = $request->development_photo_sample_sent_actual;
+            $sampleApproval->development_photo_sample_dispatch_details = $request->development_photo_sample_dispatch_details;
+            $sampleApproval->fit_approval_plan = $request->fit_approval_plan;
+            $sampleApproval->fit_approval_actual = $request->fit_approval_actual;
+            $sampleApproval->fit_sample_dispatch_details = $request->fit_sample_dispatch_details;
+            $sampleApproval->fit_sample_dispatch_sending_date = $request->fit_sample_dispatch_sending_date;
+            $sampleApproval->fit_sample_dispatch_aob_number = $request->fit_sample_dispatch_aob_number;
+            $sampleApproval->size_set_approval_plan = $request->size_set_approval_plan;
+            $sampleApproval->size_set_approval_actual = $request->size_set_approval_actual;
+            $sampleApproval->size_set_sample_dispatch_details = $request->size_set_sample_dispatch_details;
+            $sampleApproval->size_set_sample_dispatch_sending_date = $request->size_set_sample_dispatch_sending_date;
+            $sampleApproval->size_set_sample_dispatch_aob_number = $request->size_set_sample_dispatch_aob_number;
+            $sampleApproval->pp_approval_plan = $request->pp_approval_plan;
+            $sampleApproval->pp_approval_actual = $request->pp_approval_actual;
+            $sampleApproval->pp_sample_dispatch_details = $request->pp_sample_dispatch_details;
+            $sampleApproval->pp_sample_sending_date = $request->pp_sample_sending_date;
+            $sampleApproval->pp_sample_courier_aob_number = $request->pp_sample_courier_aob_number;
+            $sampleApproval->save();
+            $this->apiSuccess();
+            $this->data = (new SampleApprovalInformationResource($sampleApproval))->hide(["photo_sample_image","fit_sample_image","size_set_sample_image", "pp_sample_image"]);
+            return $this->apiOutput("SampleApprovalInformation Updated Successfully");
+        }catch(Exception $e){
+            return $this->apiOutput($this->getError( $e), 500);
+        }
+    }
+
+    public function show(Request $request)
+    {
+        try{
+            
+            $sampleApproval =  SampleApprovalInformation::find($request->id);
+            $this->data = (new SampleApprovalInformationResource($sampleApproval));
+            $this->apiSuccess("Sample Approval Showed Successfully");
+            return $this->apiOutput();
+
+        }catch(Exception $e){
+            return $this->apiOutput($this->getError($e), 500);
+        }
+    }
+
+    public function delete(Request $request)
+    {
+        SampleApprovalInformation::where("id", $request->id)->delete();
+        $this->apiSuccess();
+        return $this->apiOutput("Sample Approval Information Deleted Successfully", 200);
     }
 }
