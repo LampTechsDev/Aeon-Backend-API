f8b17beb172eede73a592450ce7e7fae095e7746
LamTechsDev <dev@lamptechs.com>
2023-01-15 19:06:30 +0600

    Compliance Audit API Added

--- app/Models/Compliance.php
+++ app/Models/Compliance.php
@@ -11,4 +11,16 @@ class Compliance extends Model
     public function fileInfo(){
         return $this->hasMany(ComplianceAuditUpload::class, 'complianceaudit_id');
     }
+
+    public function vendor(){
+        return $this->belongsTo(Vendor::class, 'vendor_id');
+    }
+
+    public function manufacture(){
+        return $this->belongsTo(VendorManufacturer::class, 'manufacturer_id');
+    }
+
+    public function audit(){
+        return $this->belongsTo(Admin::class, 'audit_conducted_by');
+    }
 }
