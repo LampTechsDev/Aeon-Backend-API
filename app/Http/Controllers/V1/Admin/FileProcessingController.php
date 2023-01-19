62995c028c2d25218f138f937303b80c2b6faf0a
Shahjalal <shajushahjalal@gmail.com>
2023-01-19 08:38:07 +0600

    DB Engine Change

--- app/Http/Controllers/V1/Admin/FileProcessingController.php
+++ app/Http/Controllers/V1/Admin/FileProcessingController.php
@@ -33,7 +33,7 @@ class FileProcessingController extends Controller
             if(!$pdf_response->status){
                 return $this->apiOutput($pdf_response->message, 402);
             }
-dd($pdf_response);
+
             $this->storePDFData($pdf_response->data);
             
         }catch(Exception $e){
@@ -45,13 +45,20 @@ dd($pdf_response);
      * Store PDF Data
      */
     public function storePDFData($data_list){
-
+        $buyer = $supplier = "";
+        $item_list = [];
         foreach($data_list as $list){
             if($list->name == "companyName"){
-
+                $buyer  = $list->value;
             }
             if($list->name == "companyName2"){
+                $supplier   = $list->value;
+            }
 
+            if($list->objectType == "table"){
+                foreach($list->rows as $_row){
+                    
+                }
             }
         }
     }
