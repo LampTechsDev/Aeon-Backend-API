14aeecc63e62667b104d356af09519776d6f98e5
LamTechsDev <dev@lamptechs.com>
2023-01-23 18:01:19 +0600

    Inspection Management Order Details Completed

--- app/Http/Controllers/V1/Admin/InspectionManagementOrderDetailsController.php
+++ app/Http/Controllers/V1/Admin/InspectionManagementOrderDetailsController.php
@@ -12,8 +12,27 @@ use Illuminate\Support\Facades\Validator;
 
 class InspectionManagementOrderDetailsController extends Controller
 {
+
+    public function index()
+    {
+       try{
+        //return 10;
+            // if(!PermissionController::hasAccess("group_list")){
+            //     return $this->apiOutput("Permission Missing", 403);
+            // }
+            $this->data = InspectionOrderDetailsResource::collection(InspectionManagementOrderDetails::all());
+            $this->apiSuccess("Inspection Management Order Details List Loaded Successfully");
+            return $this->apiOutput();
+
+        }catch(Exception $e){
+            return $this->apiOutput($this->getError($e), 500);
+        }
+    }
+
+
     public function store(Request $request)
     {
+        
      
         try{
 
@@ -42,5 +61,62 @@ class InspectionManagementOrderDetailsController extends Controller
         }catch(Exception $e){
             return $this->apiOutput($this->getError( $e), 500);
         }
+    }
+
+    public function update(Request $request)
+    {
+     
+        try{
+
+            // if(!PermissionController::hasAccess("group_create")){
+            //     return $this->apiOutput("Permission Missing", 403);
+            // }
+
+            $validator = Validator::make( $request->all(),[
+                // 'name'          => ["required", "min:4"],
+                // 'description'   => ["nullable", "min:4"],
+            ]);
+                
+            if ($validator->fails()) {    
+                $this->apiOutput($this->getValidationError($validator), 400);
+            }
+            DB::beginTransaction();
+
+            $inspection = InspectionManagementOrderDetails::find($request->id);
+            $inspection->critical_path_id = $request->critical_path_id;
+            $inspection->save();
+
+            DB::commit();
+            $this->apiSuccess();
+            $this->data = (new InspectionOrderDetailsResource($inspection));
+            return $this->apiOutput("Inspection Management Order Details Added Successfully");
+        }catch(Exception $e){
+            return $this->apiOutput($this->getError( $e), 500);
+        }
+    }
+
+
+     /*
+    Show
+    */
+    public function show(Request $request)
+    {
+        try{
+            
+            $inspection = InspectionManagementOrderDetails::find($request->id);
+            $this->data = (new InspectionOrderDetailsResource($inspection));
+            $this->apiSuccess("Inspection Management Order Details Show Successfully");
+            return $this->apiOutput();
+
+        }catch(Exception $e){
+            return $this->apiOutput($this->getError($e), 500);
+        }
+    }
+
+    public function delete(Request $request)
+    {
+        InspectionManagementOrderDetails::where("id", $request->id)->delete();
+        $this->apiSuccess();
+        return $this->apiOutput("Inspection Management Order Details Dleted Successfully", 200);
     }
 }
