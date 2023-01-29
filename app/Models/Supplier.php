35ba791c873cee550cf9af3471d4b71d1685fff4
Shahjalal <shajushahjalal@gmail.com>
2023-01-29 10:50:16 +0600

    Update PO

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
+    protected $fillable = [
+        'name',
+        "address",
+        "telephone"
+    ];
+
     use HasFactory;
 }
